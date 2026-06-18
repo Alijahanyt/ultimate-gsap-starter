e420531b3dc5dad17f7ab57eac40018154973aae
Ali jahan <ali.internet7@gmail.com>
2026-06-18 23:09:10 +0330

    Update script.js

---
+++ pages/05a_back-easing/script.js
@@ -0,0 +1,5 @@
+import gsap from "gsap";
+
+const tabs = document.querySelectorAll('.tab')
+const indicator = document.querySelector('.indicator')
+const indicator = document.querySelector('.indicaor')
