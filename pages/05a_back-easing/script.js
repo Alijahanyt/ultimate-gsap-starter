d6be64de09c3337205e17bf2d2c725f0ce62b5fa
Ali jahan <ali.internet7@gmail.com>
2026-06-20 03:21:28 +0330

    lesson finished

--- pages/05a_back-easing/script.js
+++ pages/05a_back-easing/script.js
@@ -7,5 +7,25 @@ const tabRow = document.querySelector('.tab-row')
 const updateINDICATOR = (target) =>{
     const tabBounds = target.getBoundingClientRect()
     const rowBounds = tabRow.getBoundingClientRect()
-    
+    const width = tabBounds.width
+    const offSet = tabBounds.left - rowBounds.left 
+
+    gsap.to(indicator, {
+        x: offSet,
+        width: width,
+        duration: 0.4,
+        ease: 'back.out(1.7)'
+    })
 }
+
+tabs.forEach((tab)=>{
+  tab.addEventListener("click", ()=>{
+    tabs.forEach((t)=>{
+        t.classList.remove('active')
+        tab.classList.add('active')
+        updateINDICATOR(tab)
+    })
+  })  
+})
+
+updateINDICATOR(document.querySelector('.tab.active'))
