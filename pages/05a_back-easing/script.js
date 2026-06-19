01ad520314590efa32a0571db4cd18efc72c0cf7
Ali jahan <ali.internet7@gmail.com>
2026-06-20 03:07:58 +0330

    Update script.js

--- pages/05a_back-easing/script.js
+++ pages/05a_back-easing/script.js
@@ -2,3 +2,10 @@ import gsap from "gsap";
 
 const tabs = document.querySelectorAll('.tab')
 const indicator = document.querySelector('.indicator')
+const tabRow = document.querySelector('.tab-row')
+
+const updateINDICATOR = (target) =>{
+    const tabBounds = target.getBoundingClientRect()
+    const rowBounds = tabRow.getBoundingClientRect()
+    
+}
