00651c0d37232c62515716ace4abaeaafb4ed3e6
Ali jahan <ali.internet7@gmail.com>
2026-06-22 02:40:18 +0330

    Update script.js

--- pages/13a_learn-set/script.js
+++ pages/13a_learn-set/script.js
@@ -1,2 +1,16 @@
 import gsap from "gsap";
 
+gsap.set('.gsap0box', {
+    opacity: 0,
+    scale: 1.5,
+    y: -100,
+
+})
+
+gsap.to('gsap-box', {
+    scale: 1,
+    y: 0,
+    duration: 0.6,
+    opacity: 1,
+    ease: 'expo.out',
+})
